a80edeff91eba16ae4669a5655a78e152842b002
adiska21 <adislan2017@gmail.com>
2022-03-27 18:25:12 +0300

    discord bot start

--- discord/test.py
+++ discord/test.py
@@ -1,5 +1,21 @@
 import discord
 
-TOKEN = "BOT_TOKEN"
-client = discord.Client()
+TOKEN = "OTU3NjUzODUzNDI2MzE1MzQ0.YkB6pw.P2RgoifCvF592nxwSWbhIDuBayU"
+
+
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+        for guild in self.guilds:
+            print(
+                f'{self.user} подключились к чату:\n'
+                f'{guild.name}(id: {guild.id})')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        await message.channel.send("Спасибо за сообщение")
+
+
+client = YLBotClient()
 client.run(TOKEN)
