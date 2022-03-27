9872d639d07eed62baaeb6209e8dd7ef769c00ff
adiska21 <adislan2017@gmail.com>
2022-03-27 19:31:26 +0300

    discord task 1 complete

---
+++ discord/task_1_Бот Случайный котик.py
@@ -0,0 +1,68 @@
+import discord
+import requests
+import json
+
+TOKEN = "OTU3NjUzODUzNDI2MzE1MzQ0.YkB6pw.P2RgoifCvF592nxwSWbhIDuBayU"
+
+
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'Подключен, готов показать вам пушистый комок счастья, по вашему усмотрению')
+        for guild in self.guilds:
+            print(
+                f'{self.user} подключились к чату:\n'
+                f'{guild.name}(id: {guild.id})')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if "кот" in message.content.lower():
+            response = requests.get("https://api.thecatapi.com/v1/images/search")
+            json_data = json.loads(response.text)
+            url = json_data[0]["url"]
+            print(url)
+            await message.channel.send("чоткий котя")
+            await message.channel.send(url)
+        elif "кош" in message.content.lower():
+            response = requests.get("https://api.thecatapi.com/v1/images/search")
+            json_data = json.loads(response.text)
+            url = json_data[0]["url"]
+            print(url)
+            await message.channel.send("чоткая котя")
+            await message.channel.send(url)
+
+        elif "пес" in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random")
+            json_data = json.loads(response.text)
+            url = json_data["message"]
+            print(url)
+            await message.channel.send("чоткий песя")
+            await message.channel.send(url)
+        elif "пёс" in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random")
+            json_data = json.loads(response.text)
+            url = json_data["message"]
+            print(url)
+            await message.channel.send("чоткий пёся")
+            await message.channel.send(url)
+        elif "псин" in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random")
+            json_data = json.loads(response.text)
+            url = json_data["message"]
+            print(url)
+            await message.channel.send("чоткая псина")
+            await message.channel.send(url)
+        elif "соба" in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random")
+            json_data = json.loads(response.text)
+            url = json_data["message"]
+            print(url)
+            await message.channel.send("чоткая собака")
+            await message.channel.send(url)
+        else:
+            await message.channel.send("Спасибо за сообщение, но мой создатель видимо написал не все возможные вариации склонений и спряжений для слов кот, пес, собака и кошка. Так что я вас не понял"
+                                       "\n\n либо вы написали что то другое")
+
+
+client = YLBotClient()
+client.run(TOKEN)
